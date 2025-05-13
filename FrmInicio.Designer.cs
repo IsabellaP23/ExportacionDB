1e9df1ce1b89565b44769ef8d0c9ead2a47354ae
IsabellaP23 <isabella.pena473@pascualbravo.edu.co>
2025-05-13 17:50:25 -0500

    Agregar archivos de proyecto.

---
+++ FrmInicio.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace ExportacionDB
+{
+    partial class FrmInicio
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGuardarTXT = new System.Windows.Forms.Button();
+            this.btnGuardarXML = new System.Windows.Forms.Button();
+            this.cbTablas = new System.Windows.Forms.ComboBox();
+            this.lblExportacion = new System.Windows.Forms.Label();
+            this.lblNombreTabla = new System.Windows.Forms.Label();
+            this.btnGuardarCSV = new System.Windows.Forms.Button();
+            this.btnCargarArchivo = new System.Windows.Forms.Button();
+            this.dtgDatos = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgDatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGuardarTXT
+            // 
+            this.btnGuardarTXT.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(124)))), ((int)(((byte)(112)))));
+            this.btnGuardarTXT.Font = new System.Drawing.Font("Mongolian Baiti", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardarTXT.Location = new System.Drawing.Point(438, 567);
+            this.btnGuardarTXT.Name = "btnGuardarTXT";
+            this.btnGuardarTXT.Size = new System.Drawing.Size(186, 43);
+            this.btnGuardarTXT.TabIndex = 14;
+            this.btnGuardarTXT.Text = "Guardar en TXT";
+            this.btnGuardarTXT.UseVisualStyleBackColor = false;
+            this.btnGuardarTXT.Click += new System.EventHandler(this.btnGuardarTXT_Click_1);
+            // 
+            // btnGuardarXML
+            // 
+            this.btnGuardarXML.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(124)))), ((int)(((byte)(112)))));
+            this.btnGuardarXML.Font = new System.Drawing.Font("Mongolian Baiti", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardarXML.Location = new System.Drawing.Point(721, 567);
+            this.btnGuardarXML.Name = "btnGuardarXML";
+            this.btnGuardarXML.Size = new System.Drawing.Size(186, 43);
+            this.btnGuardarXML.TabIndex = 15;
+            this.btnGuardarXML.Text = "Guardar en XML";
+            this.btnGuardarXML.UseVisualStyleBackColor = false;
+            this.btnGuardarXML.Click += new System.EventHandler(this.btnGuardarXML_Click);
+            // 
+            // cbTablas
+            // 
+            this.cbTablas.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbTablas.FormattingEnabled = true;
+            this.cbTablas.Location = new System.Drawing.Point(684, 132);
+            this.cbTablas.Name = "cbTablas";
+            this.cbTablas.Size = new System.Drawing.Size(253, 28);
+            this.cbTablas.TabIndex = 11;
+            this.cbTablas.SelectedIndexChanged += new System.EventHandler(this.cbTablas_SelectedIndexChanged);
+            // 
+            // lblExportacion
+            // 
+            this.lblExportacion.AutoSize = true;
+            this.lblExportacion.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(124)))), ((int)(((byte)(112)))));
+            this.lblExportacion.Font = new System.Drawing.Font("Mongolian Baiti", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblExportacion.Location = new System.Drawing.Point(372, 32);
+            this.lblExportacion.Name = "lblExportacion";
+            this.lblExportacion.Size = new System.Drawing.Size(358, 40);
+            this.lblExportacion.TabIndex = 17;
+            this.lblExportacion.Text = "Exportación de Datos";
+            // 
+            // lblNombreTabla
+            // 
+            this.lblNombreTabla.AutoSize = true;
+            this.lblNombreTabla.Font = new System.Drawing.Font("Mongolian Baiti", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombreTabla.Location = new System.Drawing.Point(680, 110);
+            this.lblNombreTabla.Name = "lblNombreTabla";
+            this.lblNombreTabla.Size = new System.Drawing.Size(257, 19);
+            this.lblNombreTabla.TabIndex = 16;
+            this.lblNombreTabla.Text = "Elija la tabla que desea visualizar:";
+            // 
+            // btnGuardarCSV
+            // 
+            this.btnGuardarCSV.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(124)))), ((int)(((byte)(112)))));
+            this.btnGuardarCSV.Font = new System.Drawing.Font("Mongolian Baiti", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardarCSV.Location = new System.Drawing.Point(150, 567);
+            this.btnGuardarCSV.Name = "btnGuardarCSV";
+            this.btnGuardarCSV.Size = new System.Drawing.Size(186, 43);
+            this.btnGuardarCSV.TabIndex = 12;
+            this.btnGuardarCSV.Text = "Guardar en CSV";
+            this.btnGuardarCSV.UseVisualStyleBackColor = false;
+            this.btnGuardarCSV.Click += new System.EventHandler(this.btnGuardarCSV_Click);
+            // 
+            // btnCargarArchivo
+            // 
+            this.btnCargarArchivo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(124)))), ((int)(((byte)(112)))));
+            this.btnCargarArchivo.Font = new System.Drawing.Font("Mongolian Baiti", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCargarArchivo.Location = new System.Drawing.Point(53, 120);
+            this.btnCargarArchivo.Name = "btnCargarArchivo";
+            this.btnCargarArchivo.Size = new System.Drawing.Size(158, 43);
+            this.btnCargarArchivo.TabIndex = 10;
+            this.btnCargarArchivo.Text = "Cargar archivo";
+            this.btnCargarArchivo.UseVisualStyleBackColor = false;
+            this.btnCargarArchivo.Click += new System.EventHandler(this.btnCargarArchivo_Click);
+            // 
+            // dtgDatos
+            // 
+            this.dtgDatos.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(232)))), ((int)(((byte)(220)))));
+            this.dtgDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgDatos.Location = new System.Drawing.Point(53, 179);
+            this.dtgDatos.Name = "dtgDatos";
+            this.dtgDatos.RowHeadersWidth = 51;
+            this.dtgDatos.RowTemplate.Height = 24;
+            this.dtgDatos.Size = new System.Drawing.Size(935, 330);
+            this.dtgDatos.TabIndex = 13;
+            this.dtgDatos.TabStop = false;
+            // 
+            // FrmInicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lavender;
+            this.ClientSize = new System.Drawing.Size(1038, 643);
+            this.Controls.Add(this.btnGuardarTXT);
+            this.Controls.Add(this.btnGuardarXML);
+            this.Controls.Add(this.cbTablas);
+            this.Controls.Add(this.lblExportacion);
+            this.Controls.Add(this.lblNombreTabla);
+            this.Controls.Add(this.btnGuardarCSV);
+            this.Controls.Add(this.btnCargarArchivo);
+            this.Controls.Add(this.dtgDatos);
+            this.Name = "FrmInicio";
+            this.Text = "Inicio";
+            ((System.ComponentModel.ISupportInitialize)(this.dtgDatos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGuardarTXT;
+        private System.Windows.Forms.Button btnGuardarXML;
+        private System.Windows.Forms.ComboBox cbTablas;
+        private System.Windows.Forms.Label lblExportacion;
+        private System.Windows.Forms.Label lblNombreTabla;
+        private System.Windows.Forms.Button btnGuardarCSV;
+        private System.Windows.Forms.Button btnCargarArchivo;
+        public System.Windows.Forms.DataGridView dtgDatos;
+    }
+}
+
---
+++ FrmInicio.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using System.Xml;
+using System.IO;
+
+namespace ExportacionDB
+{
+    public partial class FrmInicio : Form
+    {
+        private string dbPath = string.Empty;
+        private SQLiteConnection connection;
+        private DataTable currentData;
+
+        public FrmInicio()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCargarArchivo_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Archivos SQLite (*.db;*.sqlite;*.sqlite3)|*.db;*.sqlite;*.sqlite3|Todos los archivos (*.*)|*.*";
+                openFileDialog.Title = "Seleccionar Base de Datos SQLite";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        dbPath = openFileDialog.FileName;
+
+                        // Cerrar conexión previa si existe
+                        if (connection != null && connection.State == ConnectionState.Open)
+                        {
+                            connection.Close();
+                            connection.Dispose();
+                        }
+
+                        // Crear nueva conexión
+                        connection = new SQLiteConnection($"Data Source={dbPath};Version=3;");
+                        connection.Open();
+
+                        // Cargar las tablas disponibles
+                        LoadTables();
+
+                        cbTablas.Enabled = true;
+
+                        // Mostrar nombre del archivo en el título
+                        this.Text = $"Exportación de Datos - {Path.GetFileName(dbPath)}";
+
+                        MessageBox.Show($"Base de datos cargada correctamente: {Path.GetFileName(dbPath)}",
+                            "Base de datos cargada", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Error al abrir la base de datos: {ex.Message}", "Error",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void LoadTables()
+        {
+            // Limpiar combobox
+            cbTablas.Items.Clear();
+
+            // Consultar las tablas disponibles
+            DataTable tables = connection.GetSchema("Tables");
+
+            // Agregar las tablas al combobox
+            foreach (DataRow row in tables.Rows)
+            {
+                string tableName = row["TABLE_NAME"].ToString();
+                if (!tableName.StartsWith("sqlite_")) // Ignorar tablas del sistema
+                {
+                    cbTablas.Items.Add(tableName);
+                }
+            }
+
+            if (cbTablas.Items.Count > 0)
+            {
+                cbTablas.SelectedIndex = 0;
+            }
+        }
+
+        private void cbTablas_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbTablas.SelectedItem != null)
+            {
+                LoadTableData(cbTablas.SelectedItem.ToString());
+            }
+        }
+
+        private void LoadTableData(string tableName)
+        {
+            try
+            {
+                if (connection == null || connection.State != ConnectionState.Open)
+                {
+                    MessageBox.Show("No hay conexión a la base de datos.", "Error",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                string query = $"SELECT * FROM [{tableName}]";
+                SQLiteCommand command = new SQLiteCommand(query, connection);
+                SQLiteDataAdapter adapter = new SQLiteDataAdapter(command);
+
+                currentData = new DataTable();
+                adapter.Fill(currentData);
+
+                dtgDatos.DataSource = currentData;
+
+                btnGuardarCSV.Enabled = true;
+                btnGuardarTXT.Enabled = true;
+                btnGuardarXML.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al cargar la tabla: {ex.Message}", "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnGuardarCSV_Click(object sender, EventArgs e)
+        {
+            if (currentData == null || currentData.Rows.Count == 0)
+            {
+                MessageBox.Show("No hay datos para exportar.", "Aviso",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            ExportToFile("csv");
+        }
+
+        private void btnGuardarTXT_Click_1(object sender, EventArgs e)
+        {
+            if (currentData == null || currentData.Rows.Count == 0)
+            {
+                MessageBox.Show("No hay datos para exportar.", "Aviso",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            ExportToFile("txt");
+        }
+
+        private void btnGuardarXML_Click(object sender, EventArgs e)
+        {
+            if (currentData == null || currentData.Rows.Count == 0)
+            {
+                MessageBox.Show("No hay datos para exportar.", "Aviso",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            ExportToFile("xml");
+        }
+
+        private void ExportToFile(string fileType)
+        {
+            if (cbTablas.SelectedItem == null) return;
+
+            string tableName = cbTablas.SelectedItem.ToString();
+            string defaultFileName = $"{tableName}.{fileType}";
+
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                switch (fileType.ToLower())
+                {
+                    case "csv":
+                        saveFileDialog.Filter = "Archivos CSV (*.csv)|*.csv";
+                        break;
+                    case "txt":
+                        saveFileDialog.Filter = "Archivos de texto (*.txt)|*.txt";
+                        break;
+                    case "xml":
+                        saveFileDialog.Filter = "Archivos XML (*.xml)|*.xml";
+                        break;
+                }
+
+                saveFileDialog.FileName = defaultFileName;
+                saveFileDialog.Title = $"Guardar como {fileType.ToUpper()}";
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        string path = saveFileDialog.FileName;
+
+                        switch (fileType.ToLower())
+                        {
+                            case "csv":
+                                ExportToCSV(path);
+                                break;
+                            case "txt":
+                                ExportToTXT(path);
+                                break;
+                            case "xml":
+                                ExportToXML(path);
+                                break;
+                        }
+
+                        MessageBox.Show($"Archivo {fileType.ToUpper()} generado correctamente en:\n{path}",
+                            "Exportación exitosa", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Error al exportar los datos: {ex.Message}", "Error",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void ExportToCSV(string path)
+        {
+            using (StreamWriter sw = new StreamWriter(path, false, Encoding.UTF8))
+            {
+                // Escribir cabeceras
+                for (int i = 0; i < currentData.Columns.Count; i++)
+                {
+                    sw.Write(currentData.Columns[i].ColumnName);
+                    if (i < currentData.Columns.Count - 1)
+                    {
+                        sw.Write(";");
+                    }
+                }
+                sw.WriteLine();
+
+                foreach (DataRow row in currentData.Rows)
+                {
+                    for (int i = 0; i < currentData.Columns.Count; i++)
+                    {
+                        string value = row[i].ToString();
+                        if (value.Contains(";") || value.Contains("\"") || value.Contains("\n"))
+                        {
+                            value = $"\"{value.Replace("\"", "\"\"")}\"";
+                        }
+                        sw.Write(value);
+                        if (i < currentData.Columns.Count - 1)
+                        {
+                            sw.Write(";");
+                        }
+                    }
+                    sw.WriteLine();
+                }
+            }
+        }
+
+        private void ExportToTXT(string path)
+        {
+            using (StreamWriter sw = new StreamWriter(path, false, Encoding.UTF8))
+            {
+                int[] columnWidths = new int[currentData.Columns.Count];
+
+                for (int i = 0; i < currentData.Columns.Count; i++)
+                {
+                    columnWidths[i] = currentData.Columns[i].ColumnName.Length;
+
+                    foreach (DataRow row in currentData.Rows)
+                    {
+                        int length = row[i].ToString().Length;
+                        if (length > columnWidths[i])
+                        {
+                            columnWidths[i] = length;
+                        }
+                    }
+                }
+
+                // Escribir cabeceras
+                for (int i = 0; i < currentData.Columns.Count; i++)
+                {
+                    sw.Write(currentData.Columns[i].ColumnName.PadRight(columnWidths[i] + 2));
+                }
+                sw.WriteLine();
+
+                for (int i = 0; i < currentData.Columns.Count; i++)
+                {
+                    sw.Write(new string('-', columnWidths[i]) + "  ");
+                }
+                sw.WriteLine();
+
+                foreach (DataRow row in currentData.Rows)
+                {
+                    for (int i = 0; i < currentData.Columns.Count; i++)
+                    {
+                        sw.Write(row[i].ToString().PadRight(columnWidths[i] + 2));
+                    }
+                    sw.WriteLine();
+                }
+            }
+        }
+
+        private void ExportToXML(string path)
+        {
+            currentData.WriteXml(path, XmlWriteMode.WriteSchema);
+        }
+
+    }
+}
